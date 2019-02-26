14efaa0f124cf171cbbb0bc3029fbf69a7b7e46b
fristyy <31688915+fristyy@users.noreply.github.com>
2019-02-26 17:14:02 +0800

    添加进度条显示
    添加进度条显示，添加单条下载

--- imgAioCrawler.py
+++ imgAioCrawler.py
@@ -2,7 +2,7 @@
 '''nvshenheji爬虫 asyncio异步版本
 https://www.nvshenheji.com/'''
 
-from urllib.request import urlopen,urlretrieve,Request
+from urllib.request import urlopen
 from urllib import request,error
 import os,sys,time,socket
 import random
@@ -20,36 +20,36 @@ localpath = "E:/scrapy/nvshenheji"
 pglist_lxml ='//div[@class="nr"]/table[4]//div[@class="page"]/a/@href'
 imglist_lxml = "//div[@class='img']//img/@src"
 
-#请求头
-#headers = {}
+baseurl = "https://www.nvshenheji.com"
+localpath = "E:/scrapy/nvshenheji"	
+
+pglist_lxml ='//div[@class="nr"]/table[4]//div[@class="page"]/a/@href'
+imglist_lxml = "//div[@class='img']//img/@src"
+
+Falseimg = []  #下载未成功的图片url收集
+
 headers = {#'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
            #'Accept-Encoding': 'gzip, deflate',
             #'Cache-Control': 'max-age=0',
             #'Connection': 'keep-alive',
-            #'If-None-Match': "8074a1d8816bd41:0",
             'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.139 Safari/537.36'
                }
 
-## The proxy address and port:
-#proxy_info = { 'host' : '140.227.71.235','port' : 3128 }
-#serverlist = [
-            #'104.248.161.173:8080','111.197.236.152:9999',
-            #'201.184.135.90:42162','114.7.0.158:39345','119.101.116.30:9999','62.133.171.79:35335',
-            #'49.156.35.230:58133','128.199.212.147:3128','128.199.212.147:3128','119.101.116.30:9999',
-            #'140.227.200.38:3128','185.20.115.114:57236','85.133.207.14:56728',
-            #'118.174.232.20:53450',
-            #'182.160.117.130:53281','114.7.0.158:39345'
-             #]
-print('start get proxy! please wait!')
-DT5 = proxyDT5()
-serverlist = DT5.get_proxy()
-## We create a handler for the proxy
-#proxy = '221.7.255.167' #'112.13.170.135'
-#proxy_support = request.ProxyHandler({"http":proxy})
-## We create an opener which uses this handler:
-#opener = request.build_opener(proxy_support)
-## Then we install this opener as the default opener for urllib2:
-#request.install_opener(opener)
+serverlist = [ 
+              'http://35.245.208.185:3128',  
+              'http://125.27.10.209:59790', 
+              'http://14.207.121.183:8080', 
+              'http://1.20.100.219:60095', 
+              'http://185.22.174.69:10010', 
+              'http://193.41.88.58:53281', 
+              'http://180.159.252.111:9000', 
+              'http://216.54.73.122:46564', 
+              'http://51.15.69.7:3129']
+             
+#本段代码自动获取免费代理
+#print('start get proxy! please wait!')
+#DT5 = proxyDT5()
+#serverlist = DT5.get_proxy()
 
 def proxy_supporter(proxy_list,url):
 	random.shuffle(proxy_list)
@@ -120,7 +120,7 @@ def paglistparser(pagcontent):
 def imgurlparse(pagecontent,baseurl):
 	lxmlObj = etree.HTML(pagecontent)
 	imgurllist = lxmlObj.xpath(imglist_lxml)
-	imgurllist = [imgurl.strip() for imgurl in imgurllist]
+	imgurllist = (imgurl.strip() for imgurl in imgurllist)
 	imgurllist = [baseurl+imgurl for imgurl in imgurllist]	
 	if not imgurllist:
 		print('尝试Beautifulsoup解析')
@@ -135,39 +135,37 @@ async def nvImgpageparse(pageurl,baseurl):
 	print('开始连接分页:',pageurl)
 	pagcontent = pagparser(pageurl)
 	paglist = paglistparser(pagcontent)
-	paglist = [baseurl+pageurl for pageurl in paglist]
-	print(f'共发现:{len(paglist)}张分页')
+	paglist = (baseurl+pageurl for pageurl in paglist)
+	print(f'分析完毕，共发现:{len(paglist)}张分页')
 	imglist = imgurlparse(pagcontent,baseurl)
-	#imglist = [baseurl+imgurl for imgurl in imglist]
 	if imglist:
-		conn = aiohttp.TCPConnector(limit=5)
-		async with aiohttp.ClientSession(connector=conn,headers=headers) as session:
-			_imglisttask = [imglistpasre(pageurl,session,imglist) for pageurl in paglist]	
-			imgurllist = await asyncio.gather(*_imglisttask)
+		with tqdm(total=len(paglist),ncols=70) as pbar:  #分析进度显示
+			conn = aiohttp.TCPConnector(limit=5)
+			async with aiohttp.ClientSession(connector=conn,headers=headers) as session:
+				_imglisttask = [imglistpasre(pageurl,session,imglist,pbar) for pageurl in paglist]	
+				imgurllist = await asyncio.gather(*_imglisttask)
 		print(f'共发现:{len(imglist)}张图片')
 	return imglist
-	
-async def imglistpasre(pageurl,session,imglist):
+
+#获取分页图片列表	
+async def imglistpasre(pageurl,session,imglist,pbar):
 	maxTryNum = 5
+	proxy = random.choice(serverlist)
 	for tries in range(maxTryNum):
 		try:
-			print("连接网页:%s"%pageurl)
-			proxy = random.choice(serverlist)
-			proxy = 'http://'+proxy
-			print(f'使用代理{proxy}连接分页')
 			async with session.get(pageurl,headers=headers,timeout=10,proxy = proxy ) as req:  
 				pagcontent = await req.read()
 				imglist.extend(imgurlparse(pagcontent,baseurl))  
+			pbar.update(1)
 			break
 		except Exception as e:
-			print(e)
+			print(e) 
 			if tries < (maxTryNum-1):
-				print("第%d次尝试再次连接网页"%(tries+2))
 				await asyncio.sleep(3)
-				print("重新链接%s"%pageurl)
 				continue
 			else:
 				print("页面连接下载超时!")
+				sys.exit(1)
 
 	
 #创建文件夹
@@ -177,51 +175,67 @@ def mk_dir(localpath,title):
 		os.makedirs(dirpath)
 		print(dirpath,"创建成功")
 		return dirpath
+	elif not os.listdir(dirpath):
+		print(dirpath,"空目录已存在")
+		return dirpath
 	else:
 		print(dirpath,"目录已存在")
 		return 0
 
 #保存图片，委派生成器	
 async def savemany(imglist,dirpath):
-	conn = aiohttp.TCPConnector(limit=5)
-	imgalt = [imgurl[-15:] for imgurl in imglist] 
+	conn = aiohttp.TCPConnector(limit=10)
+	rerul = re.compile('/\w+.jpg')
+	imgalt = [rerul.findall(imgurl)[-1] for imgurl in imglist] 
 	imgitem = zip(imglist,imgalt)
-	async with aiohttp.ClientSession(connector=conn) as session:
-		_save = [imgsave(imgurl,imgalt,dirpath,session) for imgurl,imgalt in imgitem]
-		await asyncio.gather(*_save) #多任务，注意*
+	print('开始下载图片>>>')
+	with tqdm(total=len(imgalt),ncols=70) as pbar:
+		async with aiohttp.ClientSession(connector=conn) as session:
+			_save = [imgsave(imgurl,imgalt,dirpath,session, pbar) for imgurl,imgalt in imgitem]
+			await asyncio.gather(*_save) #多任务，注意*
+	for falseimg in Falseimg:
+		print(f'下载失败图片{falseimg}')
 	
 		
 #保存图片，委派生成器
-async def imgsave(imgurl,imgalt,dirpath,session):
-	print("开始连接图片",end="\t")
-	img = await imgdowload(imgurl,session) 
+async def imgsave(imgurl,imgalt,dirpath,session, pbar):
+	try:
+		img = await imgdowload(imgurl,session) 
+	except Exception as e:
+		print('开始连接图片时发生错误',e)
 	if img:
-		imgpath = dirpath+'/'+imgalt
-		print("开始保存,alt='{}'".format(imgalt))
-		with open(imgpath,'wb') as f:
-			f.write(img)
-			f.flush()
-		return imgalt
+		try:
+			imgpath = dirpath+'/'+imgalt
+			#print("开始保存,alt='{}'".format(imgalt))
+			with open(imgpath,'wb') as f:
+				f.write(img)
+				f.flush()
+			pbar.update(1)
+			return imgalt
+		except Exception as e:
+			print('保存时出现错误',e)
+			sys.exit()
 
 #异步下载图片,子生成器
 async def imgdowload(imgurl,session,headers=headers):
 	maxTryNum = 3
+	#proxy = random.choice(serverlist)
 	for tries in range(maxTryNum):
 		try:
-			print("下载图片:%s"%imgurl)
-			async with session.get(imgurl,headers=headers,timeout=20) as req:  
+			async with session.get(imgurl,timeout=20) as req: 
 				img = await req.read()
 			return img   
 		except Exception as e: 
-			print(e)
+			#print('图片下载时发生错误:',e)
 			if tries < (maxTryNum-1):
-				print("第%d次尝试再次连接图片链接"%(tries+2))
+				#print("第%d次尝试再次连接图片链接"%(tries+2))
 				await asyncio.sleep(3)
-				print("重新链接%s"%imgurl)
 				continue
-	print("图片连接下载超时!")
-	return False
+	#print("图片连接下载超时!")
+	Falseimg.append(imgurl)
+	return 0
 
+#主页分析，主获取主题列表 
 def mainpageparse(baseurl):
 	print('开始链接主页:',baseurl)
 	mainpageContent = pagparser(baseurl)
@@ -231,7 +245,8 @@ def mainpageparse(baseurl):
 	titlelist = lxmlObj.xpath("//div[@class='dan']/a/@title")	
 	conlist = list(zip(urllist,titlelist))
 	return conlist 
-	
+
+#自动检索首页推荐栏全部主题，并判断是否已下载过。	
 async def main(startpag,endpage,mainurl,localpath):	
 	conlist = mainpageparse(mainurl)
 	print(f'当前主页展示全部主题{len(conlist)}')
@@ -244,25 +259,45 @@ async def main(startpag,endpage,mainurl,localpath):
 	for pagurl,title in conlist[startpag:endpage]:
 		print('start try save theme%d:%s'%(pagnum,title))
 		print('start parse:%s'%pagurl)
+		dirpath = mk_dir(localpath,title)
+		if not dirpath:
+			print('该主题可能已下载')
+			break
 		imglist = await nvImgpageparse(pagurl,baseurl)
 		if imglist:
-			try:
-				dirpath = mk_dir(localpath,title)
-				if dirpath:
-					await savemany(imglist,dirpath)#并发下载图片
-				else:
-					print("该主题可能已下载")
-					break
+			
+			try:	
+				await savemany(imglist,dirpath)#并发下载图片
 			except Exception as e:
-				print(e)
+				print('下载保存时错误',e)
 				break
 		else:
 			print("解析可能出现错误，未发现图片")
 			continue
 		pagnum += 1
 	print('finish work!total theme:',pagnum)
+	sys.exit()
 
+#单独下载某主题
+async def subTheme(themeurl,title):
+	dirpath = mk_dir(localpath,title)
+	if not dirpath:
+		print('该主题可能已下载')
+		sys.exit(0)
+	imglist = await nvImgpageparse(themeurl,baseurl)
+	if imglist:
+		try:
+			await savemany(imglist,dirpath) #并发下载图片
+		except Exception as e:
+			print(e)
+			sys.exit(0)
+	else:
+		print("解析可能出现错误，未发现图片")
+		sys.exit(0)
+	print('finish work!total theme:')	
 
 if __name__ == '__main__':
-	#本地存放地址
-	asyncio.run(main(0,None,baseurl,localpath))
+	#启动主页下载
+	#asyncio.run(main(0,None,baseurl,localpath))	
+	#启动单独下载
+	asyncio.run(subTheme('https://www.nvshenheji.com/Candy/linmeihuizi_ef89683e.html','[Candy糖果画报]Vol.046_嫩模林美惠子Mieko沙巴旅拍蓝色紧身泳衣完美曲线写真51P'))
